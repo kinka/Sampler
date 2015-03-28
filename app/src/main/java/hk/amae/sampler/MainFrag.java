10c0f9e01fdad544bb9e8e85186cf956521e83e3
Kinka Huang <kinkabrain@gmail.com>
2015-03-28 09:43:38 +0800

    运行按钮支持三态操作

--- app/src/main/java/hk/amae/sampler/MainFrag.java
+++ app/src/main/java/hk/amae/sampler/MainFrag.java
@@ -5,13 +5,20 @@ import android.app.Activity;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Handler;
+import android.os.Message;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import hk.amae.util.Comm;
 import hk.amae.widget.TextProgressBar;
@@ -20,7 +27,7 @@ import hk.amae.widget.TextProgressBar;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MainFrag extends Fragment implements View.OnClickListener {
+public class MainFrag extends Fragment implements View.OnClickListener, View.OnTouchListener {
     OnMainFragListerer mCallback;
 
     Spinner spinChannel;
@@ -28,9 +35,10 @@ public class MainFrag extends Fragment implements View.OnClickListener {
     TextProgressBar progSampling;
     Activity parent;
     ImageButton btnLock;
+    ImageButton btnRun;
 
+    private int runningState = -1; // -1 停止 0 暂停 1 运行
     private boolean isLocked = false;
-    private boolean isRunning = false;
     private boolean isCharging = false;
 
     @Override
@@ -51,7 +59,10 @@ public class MainFrag extends Fragment implements View.OnClickListener {
         progSampling = (TextProgressBar) v.findViewById(R.id.prog_sampling);
         btnLock = (ImageButton) v.findViewById(R.id.toggle_lock);
         btnLock.setOnClickListener(this);
-        v.findViewById(R.id.toggle_run).setOnClickListener(this);
+
+        btnRun = (ImageButton) v.findViewById(R.id.toggle_run);
+        btnRun.setOnTouchListener(this);
+
         v.findViewById(R.id.btn_setting).setOnClickListener(this);
         v.findViewById(R.id.btn_connect).setOnClickListener(this);
         v.findViewById(R.id.btn_query).setOnClickListener(this);
@@ -80,14 +91,6 @@ public class MainFrag extends Fragment implements View.OnClickListener {
                 mCallback.onLockToggled(isLocked);
                 break;
 
-            case R.id.toggle_run:
-                isRunning = !isRunning;
-                if (isRunning)
-                    ((ImageButton) view).setImageResource(R.drawable.pause);
-                else
-                    ((ImageButton) view).setImageResource(R.drawable.play);
-                break;
-
             case R.id.btn_setting:
             case R.id.btn_connect:
             case R.id.btn_query:
@@ -95,6 +98,98 @@ public class MainFrag extends Fragment implements View.OnClickListener {
                 mCallback.onButtonClick(view.getId());
                 break;
         }
+    }
+
+    TimerTask poweroffTask = null;
+    TimerTask clickTask = null;
+    int clickCnt = 0;
+    long lastupTime = 0;
+    long touchStartTime = 0;
+
+    private Timer btnRunTimer = new Timer();
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 1:
+                    Toast.makeText(parent, "关机提醒", Toast.LENGTH_SHORT).show();
+                    break;
+                case 2:
+                    if (runningState == 1) {
+                        runningState = 0;
+                        btnRun.setImageResource(R.drawable.pause);
+                    } else {
+                        runningState = 1;
+                        btnRun.setImageResource(R.drawable.play);
+                    }
+                    break;
+            }
+        }
+    };
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+
+        switch (view.getId()) {
+            case R.id.toggle_run:
+                if (poweroffTask == null)
+                    poweroffTask = new TimerTask() {
+                        @Override
+                        public void run() {
+                            Message msg = new Message();
+                            msg.what = 1;
+                            handler.sendMessage(msg);
+                            poweroffTask = null;
+                        }
+                    };
+                if (clickTask == null)
+                    clickTask = new TimerTask() {
+                        @Override
+                        public void run() {
+                            Message msg = new Message();
+                            msg.what = 2;
+                            handler.sendMessage(msg);
+                            clickTask = null;
+                            clickCnt = 0;
+                        }
+                    };
+
+                final int CLICKGAP = 150, DBLCLICKGAP = 150;
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    btnRunTimer.schedule(poweroffTask, 3000);
+
+                    touchStartTime = System.currentTimeMillis();
+
+                    Comm.logI("gap " + (touchStartTime - lastupTime));
+                    if (clickCnt==1 && touchStartTime - lastupTime < DBLCLICKGAP) { // 触发双击
+                        clickCnt++;
+                        clickTask.cancel();
+                        clickTask = null;
+                        runningState = -1;
+                        btnRun.setImageResource(R.drawable.stop);
+                    }
+                } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+
+                    long touchEndTime = System.currentTimeMillis();
+                    Comm.logI("passed time " + (touchEndTime - touchStartTime));
+                    if (touchEndTime - touchStartTime < 3000) {
+                        poweroffTask.cancel();
+                        poweroffTask = null;
+                    }
+
+                    if (touchEndTime - touchStartTime <= CLICKGAP) { // click
+                        clickCnt++;
+                        if (clickCnt == 1)
+                            btnRunTimer.schedule(clickTask, DBLCLICKGAP);
+                        else
+                            clickCnt = 0;
+                    }
+
+                    lastupTime = System.currentTimeMillis();
+                }
+                break;
+        }
+        return false;
     }
 
     public interface OnMainFragListerer {

6ca3e886a062389751d32282bb82489db903101a
Kinka Huang <kinkabrain@gmail.com>
2015-04-08 23:16:56 +0800

    通道设置 radioGroup

--- app/src/main/java/hk/amae/sampler/ChannelAct.java
+++ app/src/main/java/hk/amae/sampler/ChannelAct.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import hk.amae.frag.BacklitFrag;
@@ -15,11 +17,33 @@ import hk.amae.frag.SetPowerOffFrag;
 
 
 public class ChannelAct extends Activity {
+    private RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_channel);
+
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        final RadioButton p2 = (RadioButton) findViewById(R.id.radio_p2);
+        final RadioButton p4 = (RadioButton) findViewById(R.id.radio_p4);
+        final RadioButton p8 = (RadioButton) findViewById(R.id.radio_p8);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+
+                switch (checkedId) {
+                    case R.id.radio_p2:
+                        break;
+                    case R.id.radio_p4:
+                        break;
+                    case R.id.radio_p8:
+
+                        break;
+                }
+            }
+        });
     }
 
 }

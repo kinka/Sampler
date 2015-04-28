46988f0de71088f24c6c4fbaad21a97015b4f6da
Kinka Huang <kinkabrain@gmail.com>
2015-04-28 23:28:43 +0800

    系统信息

--- app/src/main/java/hk/amae/sampler/SysInfoAct.java
+++ app/src/main/java/hk/amae/sampler/SysInfoAct.java
@@ -2,6 +2,10 @@ package hk.amae.sampler;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import hk.amae.util.Command;
 
 
 public class SysInfoAct extends Activity {
@@ -10,6 +14,28 @@ public class SysInfoAct extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_sysinfo);
+
+        findViewById(R.id.btn_back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SysInfoAct.this.onBackPressed();
+            }
+        });
+
+        doQuery();
     }
 
+    private void doQuery() {
+        new Command(new Command.Once() {
+            @Override
+            public void done(boolean verify, Command cmd) {
+                ((TextView) findViewById(R.id.txt_model)).setText(cmd.Model);
+                ((TextView) findViewById(R.id.txt_software_ver)).setText(cmd.SoftwareVer);
+                ((TextView) findViewById(R.id.txt_hardware_ver)).setText(cmd.HardwareVer);
+                ((TextView) findViewById(R.id.txt_channel_num)).setText(cmd.ChannelCount);
+                ((TextView) findViewById(R.id.txt_channel_cap)).setText(cmd.ChannelCap);
+                ((TextView) findViewById(R.id.txt_storage)).setText(cmd.Storage);
+            }
+        }).reqSysInfo();
+    }
 }

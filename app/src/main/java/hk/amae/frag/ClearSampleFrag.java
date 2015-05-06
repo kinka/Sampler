d8f375e547cf25c5085f52dcf3bf6a886b13b299
Kinka Huang <kinkabrain@gmail.com>
2015-05-06 23:00:26 +0800

    恢复出厂和清空数据

--- app/src/main/java/hk/amae/frag/ClearSampleFrag.java
+++ app/src/main/java/hk/amae/frag/ClearSampleFrag.java
@@ -1,15 +1,27 @@
 package hk.amae.frag;
 
 
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import hk.amae.sampler.R;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class ClearSampleFrag extends Fragment {
+import hk.amae.sampler.R;
+import hk.amae.util.Comm;
+import hk.amae.util.Command;
+import hk.amae.widget.TextProgressBar;
+
+public class ClearSampleFrag extends Fragment implements DialogInterface.OnClickListener {
+
+    TextProgressBar progressBar;
+    Timer timer = new Timer();
+    final String SP_CLEAR = "clear_progress";
 
     public ClearSampleFrag() {
         // Required empty public constructor
@@ -20,6 +32,65 @@ public class ClearSampleFrag extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.frag_clear_sample, container, false);
 
+        progressBar = (TextProgressBar) v.findViewById(R.id.prog_clearing);
+
+        doClearSample(false); // 先查询，看是否仍在清空数据中。。。
+
         return v;
+    }
+
+    void doClearSample(boolean doSet) {
+        new Command(new Command.Once() {
+            @Override
+            public void done(boolean verify, Command cmd) {
+                cmd.Progress = (byte) Comm.getIntSP(SP_CLEAR);
+                if (cmd.Progress == 0) {
+                    new AlertDialog.Builder(getActivity()).setTitle("确认清空采样数据？")
+                            .setCancelable(false).setPositiveButton("清空", ClearSampleFrag.this)
+                            .setNegativeButton("取消", ClearSampleFrag.this).show();
+                } else {
+                    cycleQuery();
+                }
+            }
+        }).setClearSample(doSet);
+    }
+
+    @Override
+    public void onClick(DialogInterface dialogInterface, int i) {
+        switch (i) {
+            case DialogInterface.BUTTON_POSITIVE:
+                doClearSample(true);
+                Comm.setIntSP(SP_CLEAR, 1);
+                cycleQuery();
+                break;
+            case DialogInterface.BUTTON_NEGATIVE:
+                getActivity().onBackPressed();
+                break;
+        }
+    }
+
+    private void cycleQuery() {
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                new Command(new Command.Once() {
+                    @Override
+                    public void done(boolean verify, Command cmd) {
+                        cmd.Progress = (byte) Comm.getIntSP(SP_CLEAR);
+                        cmd.Progress += 10;
+
+                        progressBar.setProgress(cmd.Progress);
+
+                        if (cmd.Progress >= 100) {
+                            timer.cancel();
+                            cmd.Progress = 0;
+                        }
+                        Comm.setIntSP(SP_CLEAR, cmd.Progress);
+
+                    }
+                }).setClearSample(false);
+            }
+        }, 0, 1000);
     }
 }
--- app/src/main/java/hk/amae/frag/RestoreFrag.java
+++ app/src/main/java/hk/amae/frag/RestoreFrag.java
@@ -1,17 +1,29 @@
 package hk.amae.frag;
 
 
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import hk.amae.sampler.R;
-import hk.amae.widget.AmaeDateTimePicker;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class RestoreFrag extends Fragment {
+import hk.amae.sampler.R;
+import hk.amae.util.Comm;
+import hk.amae.util.Command;
+import hk.amae.widget.AmaeDateTimePicker;
+import hk.amae.widget.TextProgressBar;
+
+public class RestoreFrag extends Fragment implements DialogInterface.OnClickListener {
+
+    TextProgressBar progressBar;
+    Timer timer = new Timer();
+    final String SP_RESTORE = "restore_progress";
 
     public RestoreFrag() {
         // Required empty public constructor
@@ -22,6 +34,65 @@ public class RestoreFrag extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.frag_restore, container, false);
 
+        progressBar = (TextProgressBar) v.findViewById(R.id.prog_restoring);
+
+        doRestore(false); // 先查询，看是否仍在恢复中。。。
+
         return v;
+    }
+
+    void doRestore(boolean doSet) {
+        new Command(new Command.Once() {
+            @Override
+            public void done(boolean verify, Command cmd) {
+                cmd.Progress = (byte) Comm.getIntSP(SP_RESTORE);
+                if (cmd.Progress == 0) {
+                    new AlertDialog.Builder(getActivity()).setTitle("确认恢复出厂设置？")
+                            .setCancelable(false).setPositiveButton("清空", RestoreFrag.this)
+                            .setNegativeButton("取消", RestoreFrag.this).show();
+                } else {
+                    cycleQuery();
+                }
+            }
+        }).setClearSample(doSet);
+    }
+
+    @Override
+    public void onClick(DialogInterface dialogInterface, int i) {
+        switch (i) {
+            case DialogInterface.BUTTON_POSITIVE:
+                doRestore(true);
+                Comm.setIntSP(SP_RESTORE, 1);
+                cycleQuery();
+                break;
+            case DialogInterface.BUTTON_NEGATIVE:
+                getActivity().onBackPressed();
+                break;
+        }
+    }
+
+    private void cycleQuery() {
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                new Command(new Command.Once() {
+                    @Override
+                    public void done(boolean verify, Command cmd) {
+                        cmd.Progress = (byte) Comm.getIntSP(SP_RESTORE);
+                        cmd.Progress += 10;
+
+                        progressBar.setProgress(cmd.Progress);
+
+                        if (cmd.Progress >= 100) {
+                            timer.cancel();
+                            cmd.Progress = 0;
+                        }
+                        Comm.setIntSP(SP_RESTORE, cmd.Progress);
+
+                    }
+                }).setClearSample(false);
+            }
+        }, 0, 1000);
     }
 }

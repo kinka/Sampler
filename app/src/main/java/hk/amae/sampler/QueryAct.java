06a79fa339422a3b445b98ec831a9cdc3bbfe686
Kinka Huang <kinkabrain@gmail.com>
2015-06-02 00:02:08 +0800

    查询当前状态

--- app/src/main/java/hk/amae/sampler/QueryAct.java
+++ app/src/main/java/hk/amae/sampler/QueryAct.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import hk.amae.frag.BasicInfoFrag;
 import hk.amae.util.Comm;
 import hk.amae.util.Command;
 
@@ -33,9 +34,9 @@ public class QueryAct extends Activity {
         labelTime = (TextView) findViewById(R.id.label_local_time);
         labelATM = (TextView) findViewById(R.id.label_atm);
         labelTEMP = (TextView) findViewById(R.id.label_temp);
-        labelProgress = (TextView) findViewById(R.id.label_sample_prog);
-        labelElapse = (TextView) findViewById(R.id.label_sample_time);
-        labelDuration = (TextView) findViewById(R.id.label_set_time);
+        labelProgress = (TextView) findViewById(R.id.label_sampled_prog);
+        labelElapse = (TextView) findViewById(R.id.label_sampled_time);
+        labelDuration = (TextView) findViewById(R.id.label_duration);
         labelSampleMode = (TextView) findViewById(R.id.label_sample_mode);
         labelLaunchMode = (TextView) findViewById(R.id.label_launch_mode);
         labelChannel = (TextView) findViewById(R.id.label_channel);
@@ -49,13 +50,26 @@ public class QueryAct extends Activity {
             @Override
             public void done(boolean verify, Command cmd) {
 //                cmd.TargetSpeed = 200;
+                cmd.DateTime = "2015-06-01 23:33";
                 labelSpeed.setText(String.format("%dmL/min", cmd.Speed));
                 labelTargetSpeed.setText(String.format("%dmL/min", cmd.TargetSpeed));
                 labelVolume.setText(String.format("%dmL", cmd.Volume));
                 labelStandardVol.setText(String.format("%dmL", cmd.StandardVol));
-                labelTime.setText(cmd.DateTime);
 
+                labelTime.setText(cmd.DateTime);
+                labelATM.setText(String.format(BasicInfoFrag.atmFormat, cmd.ATM));
+                labelTEMP.setText(String.format(BasicInfoFrag.tempFormat, cmd.TEMP));
+
+                labelProgress.setText(cmd.Progress + "%"); // H M S
+                labelElapse.setText(cmd.Elapse + "s");
+                labelDuration.setText(cmd.TargetDuration + "min");
+
+                labelSampleMode.setText(cmd.SampleMode == Comm.TIMED_SET_CAP ? "定容量":"定时长");
                 labelLaunchMode.setText(cmd.SampleMode == Comm.MANUAL_SET ? "手动":"定时");
+
+//                cmd.Channel = Comm.Channel.CH1; // todo 使用服务器返回值
+                labelChannel.setText(cmd.Channel == null ? "" : cmd.Channel.name());
+                labelGroup.setText(cmd.SampleMode == Comm.MANUAL_SET ? "0" : String.format("第%d组", cmd.Group));
             }
         }).reqSampleState();
     }

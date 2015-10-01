7e4ba006ad22a9d762738d9cc97c0b265429c298
Kinka <kinkabrain@gmail.com>
2015-10-01 10:00:56 +0800

    清空采样数据改造完成

--- app/src/main/java/hk/amae/frag/ClearSampleFrag.java
+++ app/src/main/java/hk/amae/frag/ClearSampleFrag.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Timer;
@@ -19,6 +20,8 @@ import hk.amae.util.Command;
 import hk.amae.widget.TextProgressBar;
 
 public class ClearSampleFrag extends Fragment implements DialogInterface.OnClickListener {
+
+    TextView lblStatus;
 
     TextProgressBar progressBar;
     Timer timer = new Timer();
@@ -35,6 +38,8 @@ public class ClearSampleFrag extends Fragment implements DialogInterface.OnClick
 
         progressBar = (TextProgressBar) v.findViewById(R.id.prog_clearing);
 
+        lblStatus = (TextView) v.findViewById(R.id.lbl_status);
+
         v.findViewById(R.id.btn_back).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -42,33 +47,35 @@ public class ClearSampleFrag extends Fragment implements DialogInterface.OnClick
             }
         });
 
-        doClearSample(false); // 先查询，看是否仍在清空数据中。。。
+        v.findViewById(R.id.btn_confirm).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(getActivity()).setTitle("确认清空采样数据？")
+                        .setCancelable(false).setPositiveButton("清空", ClearSampleFrag.this)
+                        .setNegativeButton("取消", ClearSampleFrag.this).show();
+            }
+        });
 
         return v;
     }
 
-    void doClearSample(final boolean doSet) {
+    void doClearSample() {
+        Comm.setIntSP(SP_CLEAR, 0);
         new Command(new Command.Once() {
             @Override
             public void done(boolean verify, Command cmd) {
-                int progress = Comm.getIntSP(SP_CLEAR);
-                if (progress == 0) {
-                    new AlertDialog.Builder(getActivity()).setTitle("确认清空采样数据？")
-                            .setCancelable(false).setPositiveButton("清空", ClearSampleFrag.this)
-                            .setNegativeButton("取消", ClearSampleFrag.this).show();
-                } else {
-                    cycleQuery();
-                }
+                lblStatus.setText("正在清空");
+                timer = new Timer();
+                cycleQuery();
             }
-        }).setClearSample(doSet);
+        }).setClearSample(true);
     }
 
     @Override
     public void onClick(DialogInterface dialogInterface, int i) {
         switch (i) {
             case DialogInterface.BUTTON_POSITIVE:
-                Comm.setIntSP(SP_CLEAR, 1);
-                doClearSample(true);
+                doClearSample();
                 break;
             case DialogInterface.BUTTON_NEGATIVE:
                 getActivity().onBackPressed();
@@ -87,26 +94,18 @@ public class ClearSampleFrag extends Fragment implements DialogInterface.OnClick
                         cmd.Progress += 10;
 
                         progressBar.setProgress(cmd.Progress);
+                        Comm.setIntSP(SP_CLEAR, cmd.Progress);
 
                         if (cmd.Progress >= 100) {
                             timer.cancel();
                             timer.purge();
-                            cmd.Progress = 0;
+                            lblStatus.setText("清空完成");
+                        } else {
+                            cycleQuery();
                         }
-                        Comm.setIntSP(SP_CLEAR, cmd.Progress);
                     }
                 }).setClearSample(false);
             }
-        }, 0, 1000);
-    }
-
-    @Override
-    public void onPause() {
-        try {
-            timer.cancel();
-        } catch (Exception e) {
-
-        }
-        super.onPause();
+        }, 100);
     }
 }
